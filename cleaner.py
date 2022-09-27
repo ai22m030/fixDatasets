8bbdc51be725e143449174af3c2c8e85e1b2cb8b
adva <adnan.vatric@jumptoday.group>
2022-09-27 05:36:24 +0200

    raw data correction finished

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,6 @@
 import pandas as pd
+import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from collections import Counter
 
@@ -16,32 +16,46 @@ class DataCorrector:
         self.filepath = filepath
         file = open(filepath, 'r')
         self.unparsed_lines = file.readlines()
+        self.data = None
 
     def step_01_clean_raw_file(self):
-        result_step_1 = []
-        result_step_2 = pd.DataFrame()
+        raw_data = []
+        prepared_data = pd.DataFrame()
+        bad_rows = pd.DataFrame()
 
         for line in self.unparsed_lines:
             if ";" in line or "," in line:
-                result_step_1.append(line)
+                raw_data.append(line)
 
-        for line in result_step_1:
+        for line in raw_data:
             semicolon_count = sum(v for k, v in Counter(line).items() if k in ';')
             comma_count = sum(v for k, v in Counter(line).items() if k in ',')
             tmp = pd.DataFrame(data=[[line, comma_count, semicolon_count]], columns=['line', 'commas', 'semicolons'])
-            result_step_2 = pd.concat([result_step_2, tmp], ignore_index=True)
+            prepared_data = pd.concat([prepared_data, tmp], ignore_index=True)
 
+        if np.any(prepared_data.iloc[:, 2] != int(np.quantile(prepared_data.iloc[:, 2], 0.75))) == np.bool_(
+                True) or np.any(
+                prepared_data.iloc[:, 1] != int(np.quantile(prepared_data.iloc[:, 1], 0.75))) == np.bool_(True):
+            bad_rows = pd.concat(
+                [prepared_data[prepared_data.semicolons != int(np.quantile(prepared_data.iloc[:, 2], 0.75))],
+                 prepared_data[
+                     prepared_data.commas != int(np.quantile(prepared_data.iloc[:, 1], 0.75))]]).drop_duplicates()
 
+        for index, row in bad_rows.iterrows():
+            if row.semicolons != int(np.quantile(prepared_data.semicolons, 0.75)):
+                prepared_data.at[index, 'line'] = row.line.replace(',', ';')
+                prepared_data.at[index, 'semicolons'] = int(np.quantile(prepared_data.semicolons, 0.75))
+                prepared_data.at[index, 'commas'] = 0
+            elif row.commas != int(np.quantile(prepared_data.commas, 0.75)):
+                prepared_data.at[index, 'line'] = row.line.replace(',', '.')
+                prepared_data.at[index, 'commas'] = 0
 
-        print(result_step_2.describe())
-        print(result_step_2[result_step_2.semicolons == 0 and result_step_2.commas == 0])
-        plt.hist(result_step_2)
+        filewriter = open(self.filepath.replace('.csv', '.tmp.csv'), 'w')
+        filewriter.writelines(prepared_data.line)
+        filewriter.close()
+
+    def step_02_load_and_check_types(self):
+        self.data = pd.read_csv(self.filepath.replace('.csv', '.tmp.csv'), delimiter=';')
+
+        self.data.hist()
         plt.show()
-
-        # print(result_step_2.get(['semicolons']).max())
-        # exit(0)
-
-        # write_file = open('new_' + self.filepath, 'w')
-        # write_file.writelines(result_step_1)
-        # write_file.close()
-
--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 from cleaner import DataCorrector
-import pandas as pd
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    '''
-    data = pd.read_csv('assignment_datahandling_wine_exercise.csv', skiprows=[0, 52, 53, 144], header=0, skipfooter=1,
-                       engine='python', delimiter=';')
-    print(data.head())
-    print(data.columns)
-    print(data.index)
-    print(data.dtypes)
-    print(data.alcohol.hist)
-    print(data.describe())
-    plt.hist(data)
-    plt.title("Histogram mit default Anzahl an Bins")
-    plt.xlabel("PS")
-    plt.ylabel("Häufigkeit")
-    plt.show()
-    '''
     parser = DataCorrector('assignment_datahandling_wine_exercise.csv')
     parser.step_01_clean_raw_file()
+    parser.step_02_load_and_check_types()
